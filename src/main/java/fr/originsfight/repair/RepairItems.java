2b83afd468cdfce78517572871116acaceb8421f
blixoww <valentin.houpert@free.fr>
2024-11-12 00:18:36 +0100

    update

--- src/main/java/fr/originsfight/repair/RepairItems.java
+++ src/main/java/fr/originsfight/repair/RepairItems.java
@@ -8,15 +8,20 @@ import java.util.Objects;
 public class RepairItems {
 
     public static boolean canRepair(ItemStack itemStack) {
-        return itemStack.getDurability() > 0;
+        return itemStack.getType().getMaxDurability() > 0 && itemStack.getDurability() > 0;
     }
 
     public static boolean repair(ItemStack[] items) {
         if (items == null) return false;
-        return Arrays.stream(items)
-                .filter(Objects::nonNull)
-                .filter(RepairItems::canRepair)
-                .peek(a -> a.setDurability((short) 0))
-                .count() > 0;
+        boolean anyItemRepaired = false;
+
+        for (ItemStack item : items) {
+            if (item != null && canRepair(item)) {
+                item.setDurability((short) 0);
+                anyItemRepaired = true;
+            }
+        }
+
+        return anyItemRepaired;
     }
 }
